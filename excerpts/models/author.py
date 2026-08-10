234cf571a89f629a6986e54472e189d03d40f83f
joerisner <joe.risner@proton.me>
2026-08-10 17:13:39 -0600

    Remove explicit nullable arg in lieu of Mapped annotation

--- excerpts/models/author.py
+++ excerpts/models/author.py
@@ -8,4 +8,4 @@ class Author(Base):
     __tablename__ = "authors"
 
     first_name: Mapped[str | None] = mapped_column(String(50))  # Nullable allows for mononyms.
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50))
