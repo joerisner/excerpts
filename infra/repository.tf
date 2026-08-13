4e46d4fac73540a59759a5bf214cc4d7066fdfa9
joerisner <joe.risner@proton.me>
2026-08-13 15:53:52 -0600

    Remove extraneous todo in tf

--- infra/repository.tf
+++ infra/repository.tf
@@ -81,7 +81,6 @@ resource "github_actions_repository_permissions" "this" {
   repository      = github_repository.this.name
   allowed_actions = "selected"
 
-  # TODO: No CI workflow created yet, but will be added later.
   allowed_actions_config {
     github_owned_allowed = true # actions/checkout, actions/setup-*, etc.
     verified_allowed     = false
